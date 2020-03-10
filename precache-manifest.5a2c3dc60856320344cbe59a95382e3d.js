d3199b2d1ce3904b8ceda46d5991d1750c17203a
Fangyi Zhou <me@fangyi.io>
2020-03-10 02:13:15 +0000

    Updates

---
+++ precache-manifest.5a2c3dc60856320344cbe59a95382e3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dbc19702fb94f5b53a45d979aec3e73",
+    "url": "/covid19-england/index.html"
+  },
+  {
+    "revision": "e41b42dc75b04f05ce68",
+    "url": "/covid19-england/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "55b8509b7153f483261b",
+    "url": "/covid19-england/static/js/2.bd80b0c5.chunk.js"
+  },
+  {
+    "revision": "59fc29711a22a223e9f602decc11b79f",
+    "url": "/covid19-england/static/js/2.bd80b0c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e41b42dc75b04f05ce68",
+    "url": "/covid19-england/static/js/main.dd5a17c7.chunk.js"
+  },
+  {
+    "revision": "ffc6302c1404a033ba9f",
+    "url": "/covid19-england/static/js/runtime-main.48688ace.js"
+  }
+]);
